090f91464bb6b7a9f649d3893156d2544cdd906b
Ilya Minyaev <ayli.veaynim@gmail.com>
2016-04-25 01:40:14 +0300

    Activities templates added

--- app/src/main/java/com/msc/myplace/MainActivity.java
+++ app/src/main/java/com/msc/myplace/MainActivity.java
@@ -1,4 +1,4 @@
-package com.msc.familyguard;
+package com.msc.myplace;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.msc.myplace.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
